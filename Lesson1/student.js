e8a61fe279cd63b0088ad8b7566cc045d26fd50a
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-02-03 00:45:33 +0600

    submit

--- Lesson1/student.js
+++ Lesson1/student.js
@@ -1,3 +1,13 @@
 const getName = () => {
     return "Jahangir alam";
 }
+
+const getAge = () => {
+    return "22";
+}
+
+const cgpa = 3.22;
+
+exports.getName = getName;
+exports.getAge = getAge;
+exports.getCgpa = cgpa;
