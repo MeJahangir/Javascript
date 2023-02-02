33b61685a5e996e029a7779f4f55a39c429ba4f3
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-02-03 00:55:56 +0600

    push

---
+++ Lesson1 module input/export/student.js
@@ -0,0 +1,20 @@
+const getName = () => {
+    return "Jahangir alam";
+}
+
+const getAge = () => {
+    return "22";
+}
+
+const cgpa = 3.22;
+
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.getCgpa = cgpa;
+
+module.exports = {
+    getName,
+    getAge,
+    cgpa
+}
