2307964121e07462d064036b3d1c30255a1db10c
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-02-05 22:39:59 +0600

    push

--- Lesson2-user input collect/index.js
+++ Lesson2-user input collect/index.js
@@ -2,13 +2,17 @@
 // var jahangir = prompt('Enter your names:');
 // console.log('Hi...' + jahangir);
 
-var jahangir = prompt('Enter your names:');
-document.write('Hi...' + jahangir);
+// var jahangir = prompt('Enter your names:');
+// document.write('Hi...' + jahangir);
 
-let number = prompt('Enter your number..');
-console.log(number);
+// let number = prompt('Enter your number..');
+// console.log(number);
 
 var fName = 'jahangir alam';
 var lName = 'alam';
 var fullName = fName + lName;
 console.log(fullName);
+
+function myFunction() {
+    
+}
