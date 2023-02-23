b53d607a41a90be63c93d3592ec7ec083bea0f49
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-02-23 12:55:30 +0600

    push

--- Lesson2-user input collect/js/login.js
+++ Lesson2-user input collect/js/login.js
@@ -1,5 +1,13 @@
-// user name and password maching
-console.log("hello bangladesh");
+// step : 1 add even handelar with the submit button
+
 document.getElementById('btn-submit').addEventListener('click', function () {
-    console.log('submit button clicked');
+    // step : 2 get the email address
+    const emailFild = document.getElementById('user-email');
+    const email = emailFild.value;
+    // console.log(email);
+    // step : 3 get the password collect
+    const passwodFild = document.getElementById('user-password');
+    const password = passwodFild.value;
+    // console.log(password);
+    console.log(email, password);
 })
