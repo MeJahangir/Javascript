91c5f04db667542f63df36fc226e48ea24840f1c
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-02-23 12:28:00 +0600

    submit

--- Lesson2-user input collect/js/login.js
+++ Lesson2-user input collect/js/login.js
@@ -1,1 +1,5 @@
 // user name and password maching
+console.log("hello bangladesh");
+document.getElementById('btn-submit').addEventListener('click', function () {
+    console.log('submit button clicked');
+})
