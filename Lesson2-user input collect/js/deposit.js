e9ad0d0ddd903a3d5c50cffb50989f46569ec04f
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-03-31 13:04:58 +0600

    deposit area

--- Lesson2-user input collect/js/deposit.js
+++ Lesson2-user input collect/js/deposit.js
@@ -11,4 +11,27 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const currentDepositTotal = previuseDepositTotal + newDeposit;
     depositTotalElement.innerText = currentDepositTotal;
 
+    // ballance total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const balanceTotalString = balanceTotalElement.innerText;
+    const newBalanceTotal = parseFloat(balanceTotalString);
+
+    const currentBalanceTotal = newBalanceTotal + previuseDepositTotal;
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+})
+
+// withdrow area 
+document.getElementById('btn-withdrow').addEventListener('click', function () {
+    const withdrowField = document.getElementById('withdrow');
+    const withdrowFieldString = withdrowField.value;
+    const withdrow = parseFloat(withdrowFieldString);
+    
+    const withdrowTotalElement = document.getElementById('withdrow-total');
+    const previueWithdrowString = withdrowTotalElement.innerText;
+    const previusWithdrowTotal = parseFloat(previueWithdrowString);
+    
+    const currentWithdrowTotal = previusWithdrowTotal + withdrow;
+    withdrowField.innerText = currentWithdrowTotal;
+
 })
